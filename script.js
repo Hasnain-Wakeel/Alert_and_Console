963ebf408bbbd64dc0103fc36ef4bcd9ade9b5fe
Hasnain-Wakeel <wakeelanjum112@gmail.com>
2025-09-29 17:33:35 +0500

    Updating old repo

---
+++ script.js
@@ -0,0 +1,20 @@
+// Alert shows a Popup with the given message.
+
+// alert("Hasnain Raza");
+// console.log(`The type of Hasnain Raza is: ${typeof('Hasnain Raza')}`)
+
+// alert(2)
+// console.log(`The type of 2 is: ${typeof(2)}`)
+
+// let firstName = "Hasnain"
+// let lastName = "Raza"
+// alert(firstName + " " + lastName);
+
+// --------------------------------------------------------------------------------------------------
+
+// Console prints the message "OR" shows the output of code to the Developer's Mode (Area)
+
+// console.log("Hello World");
+
+// let a = 11;
+// console.log(a);
